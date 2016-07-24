cb98283eca0c1f9bb36c1c60daebb807150a727d
HL <lecaoquochung@gmail.com>
2016-07-24 23:53:24 +0900

    gulpfile config js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require("gulp-rename");
 var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
 
+// css
 gulp.task('css', function() {
   gulp.src('./less/styles.less')
   .pipe(less({
@@ -19,4 +23,12 @@ gulp.task('watchers', function() {
   gulp.watch('less/**/*.less', ['css']);
 });
 
-gulp.task('default', ['css', 'watchers'])
+// javascript
+gulp.task('js', function() {
+  gulp.src('./js/app.js')
+  .pipe(browserify())
+  .pipe(gulp.dest('./static/js'))
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./static/js'))
+});
