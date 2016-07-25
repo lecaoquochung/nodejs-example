0ec4c373d2223aa3f3d487380b9a1b6582533f36
hungle <lecaoquochung@gmail.com>
2016-07-25 13:11:35 +0900

    MVC layers: init Router.js

---
+++ frontend/js/lib/Router.js
@@ -0,0 +1,12 @@
+// frontend/js/lib/Router.js
+module.exports = function() {
+  return {
+    routes: [],
+    add: function(path, handler) {
+      // ...
+    },
+    check: function(fragment, params) {
+      // ...
+    }
+  }
+};
